f8ce8b8c36097a3704661f4d6f5cc38f80d4edd6
WojciechSadowski <37096816+WojciechSadowski@users.noreply.github.com>
2025-08-24 20:48:20 +1000

    Fix GetApplicationName when FileDescription is not set

--- install/ExtractPrograms.ps1
+++ install/ExtractPrograms.ps1
@@ -103,9 +103,8 @@ function GetApplicationName {
         [string]$exePath
     )
 
-    try {
-        $productName = (Get-Item $exePath).VersionInfo.FileDescription.Trim() -replace '\s+', ' '
-    } catch {
+    $productName = (Get-Item $exePath).VersionInfo.FileDescription.Trim() -replace '\s+', ' '
+    if ([string]::IsNullOrEmpty($productName)) {
         $productName = [System.IO.Path]::GetFileNameWithoutExtension($exePath)
     }
 
